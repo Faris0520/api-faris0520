5f95256f9c4cb63dd955102304a9a694b9cae14a
Faris0520 <Faris0520@users.noreply.github.com>
2021-06-23 22:45:46 +0700

    api-faris

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ app.use(cors())
 app.use(secure)
 app.use(express.static("public"))
 
-app.use('/', mainrouter)
-app.use('/api', apirouter)
+app.use('/', mainrouter, apirouter)
 
 app.listen(PORT, () => {
     console.log(color("Server running on port " + PORT,'green'))
